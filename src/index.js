dd1e3c06c13d40b9166467eceaf1ddb4456a8d19
Sam Cymbaluk <samcymbaluk@gmail.com>
2016-12-30 17:19:37 -0500

    Rest API now only allows connections from the same domain

--- src/index.js
+++ src/index.js
@@ -6,4 +6,4 @@ const server = restify.createServer();
 server.use(restify.authorizationParser());
 routes.addRoutes(server);
 
-server.listen(8080, () => console.log('%s listening at %s', server.name, server.url));
+server.listen(3001, 'localhost', () => console.log('%s listening at %s', server.name, server.url));
