57007842146d16ffdbadd8b46a3f9e1a371c5325
Sam Cymbaluk <samcymbaluk@gmail.com>
2017-01-19 19:16:33 -0500

    Fixed Cor issue

--- src/index.js
+++ src/index.js
@@ -3,8 +3,18 @@ const routes = require('./routes');
 
 const server = restify.createServer();
 
+server.use(restify.CORS());
+
+server.opts(/.*/, (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", req.header("Access-Control-Request-Method"));
+    res.header("Access-Control-Allow-Headers", req.header("Access-Control-Request-Headers"));
+    res.send(200);
+    return next();
+});
+
 server.use(restify.bodyParser({ mapParams: true }));
 
 routes.addRoutes(server);
 
-server.listen(3001, 'localhost', () => console.log(`${server.name} listening at ${server.url}`));
+server.listen(3001, () => console.log(`${server.name} listening at ${server.url}`));
